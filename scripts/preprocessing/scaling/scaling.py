4ea6da058029feb5d091f2e28192d56c34764bff
Risbourg Nathan <n.risbourg@pellencst.com>
2022-01-21 17:47:33 +0100

    Adding scaler_type parameter

--- scripts/preprocessing/scaling/scaling.py
+++ scripts/preprocessing/scaling/scaling.py
@@ -3,15 +3,17 @@ Module used to scale the numerical values of the dataset
 """
 
 import pandas as pd
-import sklearn as sk
+import sklearn.preprocessing
 
 
-def scale(dataset: pd.DataFrame, columns: list[str] = None):
+def scale(dataset: pd.DataFrame, columns: list[str] = None, scaler_type: str = "standard"):
     """
-    Scale the dataframe 'dataset' using the scikit learn standard scaler
+    Scale the dataframe 'dataset' using the specified scikit learn scaler
 
     :param dataset: DataFrame to scale
     :param columns: Optional list of the DataFrame columns to scale. Leave None to use the predefined list
+    :param scaler_type: Scaler type, must be one of 'standard', 'minmax' or 'robust'. Will use 'standard' as default
+    value or if scaler_type is invalid
     :return: Scaled DataFrame
     """
     if columns is None:
@@ -23,4 +25,16 @@ def scale(dataset: pd.DataFrame, columns: list[str] = None):
         # Use only columns that definitely exist in the dataset
         columns = list(default_columns.intersection(dataset.columns))
 
-    dataset[columns] = sk.preprocessing.StandardScaler().fit_transform(dataset[columns])
+    valid_scalers = {
+        'standard': sklearn.preprocessing.StandardScaler,
+        'minmax': sklearn.preprocessing.MinMaxScaler,
+        'robust': sklearn.preprocessing.RobustScaler
+    }
+
+    scaler = valid_scalers.get(scaler_type)
+
+    if scaler is None:
+        print(f"[WARNING]: scaler type {scaler_type} doesn't exist or is not supported, defaulting to StandardScaler")
+        scaler = valid_scalers.get('standard')
+
+    dataset[columns] = scaler().fit_transform(dataset[columns])
