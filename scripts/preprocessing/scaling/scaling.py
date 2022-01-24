ca861f89042eef792f766dfae15e244eff314cae
Risbourg Nathan <n.risbourg@pellencst.com>
2022-01-24 15:32:13 +0100

    Syntax improvement

--- scripts/preprocessing/scaling/scaling.py
+++ scripts/preprocessing/scaling/scaling.py
@@ -17,10 +17,10 @@ def scale(dataset: pd.DataFrame, columns: list[str] = None, scaler_type: str = "
     :return: Scaled DataFrame
     """
     if columns is None:
-        default_columns = frozenset(['energy_100g', 'fat_100g', 'saturated-fat_100g', 'carbohydrates_100g',
-                                     'sugars_100g', 'fiber_100g', 'proteins_100g', 'salt_100g', 'sodium_100g',
-                                     'additives_n', 'ingredients_from_palm_oil_n',
-                                     'ingredients_that_may_be_from_palm_oil_n'])
+        default_columns = {'energy_100g', 'fat_100g', 'saturated-fat_100g', 'carbohydrates_100g',
+                           'sugars_100g', 'fiber_100g', 'proteins_100g', 'salt_100g', 'sodium_100g',
+                           'additives_n', 'ingredients_from_palm_oil_n',
+                           'ingredients_that_may_be_from_palm_oil_n'}
 
         # Use only columns that definitely exist in the dataset
         columns = list(default_columns.intersection(dataset.columns))
