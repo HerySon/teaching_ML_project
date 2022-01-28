fb0e2969ec83349623305200b32b0700de5f0a1b
qtzzz <email@example.com>
2022-01-28 10:42:05 +0100

    [WIP] Changed logic + Docstring

--- scripts/preprocessing/feature_selection.py
+++ scripts/preprocessing/feature_selection.py
@@ -2,24 +2,42 @@
 The Feature Selection Module intends to realize automated tasks related to feature selection
 
 """
+from ctypes import Union
+from sklearn.feature_selection import SelectKBest, GenericUnivariateSelect
 import pandas as pd
 
 
-def feature_selection(df: pd.DataFrame):
-    """
-        Return a Dataframe containing only the columns
-        dimmed pertinent for training
-    """
-    # TODO Remove this block when data cleaning task has been merged
-    for col in df.columns:
-        sum = df[col].isna().sum()
-        if sum/len(df)*100 > 50:
-            df.drop(columns=col, inplace=True)
-    #################################################################        
-    columns = [
-    'url', 'creator', 'created_t', 'created_datetime', 'states', 'states_tags', 'last_modified_datetime', 'last_modified_t',
-    'image_url', 'image_small_url', 'image_nutrition_url', 'image_nutrition_small_url', 'countries','countries_en', 
-    'nutrition-score-fr_100g','energy_100g', 'product_name', 'states', 'states_en', 'brands', 'brands_tags'
-    ]
-    df.drop(columns=df.columns.intersection(columns), inplace=True)
 
+def feature_selection(dataframe: pd.DataFrame, columns: Union(str, list[str], None), method: str):
+    """
+        Realize various feature selection operations
+
+
+    Parameters
+    ----------
+        df: A pd.Dataframe object
+
+
+        columns: None, str or array of str
+            List of columns to drop before automated feature selection
+            If None, remaining columns will be as explained in the feature_selection notebook
+
+
+        method: {'kbest', 'genericunivariateselect'}
+            Method to use for feature selection. 
+        
+    """  
+    if columns is not None:
+        df = dataframe.drop(columns=columns)
+    else:
+        columns=['code', 'brands_tags', 'countries_tags', 'pnns_groups_1',
+        'pnns_groups_2', 'energy-kcal_100g', 'fat_100g', 'saturated-fat_100g',
+        'carbohydrates_100g', 'sugars_100g', 'proteins_100g', 'salt_100g',
+        'sodium_100g', 'nutrition-score-fr_100g']
+        try:
+            df = dataframe[columns]
+        except IndexError:
+            print('Could not instantiate a new dataframe with default columns [IndexError]')
+
+
+    
