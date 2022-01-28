ab8c0bc09f946b57e84a79f4f5b06b796e7b2576
Robi <43955990+FournierRobin@users.noreply.github.com>
2022-01-28 16:51:56 +0100

    Update scripts/preprocessing/outliers/outliers.py
    Co-authored-by: RisbourgNathan <39952585+RisbourgNathan@users.noreply.github.com>

--- scripts/preprocessing/outliers/outliers.py
+++ scripts/preprocessing/outliers/outliers.py
@@ -14,10 +14,8 @@ def outliers_del(df):
     Q1 = df.quantile(0.25)
     Q3 = df.quantile(0.75)
     IQR = Q3 - Q1
-    df_out = df[~((df < (Q1 - 1.5 * IQR)) |
-                  (df > (Q3 + 1.5 * IQR))).any(axis=1)]
-
-    return df_out
+    df.drop(df[((df < (Q1 - 1.5 * IQR)) |
+                  (df > (Q3 + 1.5 * IQR))).any(axis=1)].index, inplace=True)
 
 
 def outliers_nan(df):
