d1bdfe59831cf03f5be9d5838ee9c074ba2e9fc4
Nicolas Rochet <nicolas.rochet@protonmail.com>
2022-02-09 17:36:56 +0100

    refactoring into one fonction

--- scripts/preprocessing/outliers/outliers.py
+++ scripts/preprocessing/outliers/outliers.py
@@ -5,30 +5,27 @@ Module used to get rid of outliers in the dataset
 import pandas as pd
 
 
-def outliers_del(df):
-    """
-    Get rid of outliers using the InterQuartile Range method
+def process(df, action='replace') -> df_out:
+    """ Process outliers using the InterQuartile Range method
 
-    This function deletes the whole row where it finds an outliers
+    Args:
+        df (dataframe): input data
+        action (str, optional): processing action 
+            'replace': replace outliers by NaN values
+            'delete' : delete each outliers
+       
+    Returns:
+        df_out: processed dataframe
     """
     Q1 = df.quantile(0.25)
     Q3 = df.quantile(0.75)
     IQR = Q3 - Q1
-    df_out = df[~((df < (Q1 - 1.5 * IQR)) |
+    
+    if action == 'replace':    
+        df_out = df.mask(((df < (Q1 - 1.5 * IQR)) | (df > (Q3 + 1.5 * IQR))))
+        
+    elif action == 'delete':
+        df_out = df[~((df < (Q1 - 1.5 * IQR)) |
                   (df > (Q3 + 1.5 * IQR))).any(axis=1)]
-
-    return df_out
-
-
-def outliers_nan(df):
-    """
-    Get rid of outliers using the InterQuartile Range method
-
-    This function keeps the line and turn the outlier into a NaN value
-    """
-    Q1 = df.quantile(0.25)
-    Q3 = df.quantile(0.75)
-    IQR = Q3 - Q1
-    df_out = df.mask(((df < (Q1 - 1.5 * IQR)) | (df > (Q3 + 1.5 * IQR))))
 
     return df_out
